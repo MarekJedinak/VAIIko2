f0f543f9bd2b489af3f85adcd54da23dafeb78d2
MarekJedinak <jedinakmarek01@gmail.com>
2025-02-05 14:19:32 +0100

    Vizualne upravy

--- App/Views/Auth/login.view.php
+++ App/Views/Auth/login.view.php
@@ -1,36 +1,41 @@
 <?php
 
-$layout = 'auth';
+//$layout = 'auth';
 /** @var Array $data */
 /** @var \App\Core\LinkGenerator $link */
+/** @var string $contentHTML */
+
 ?>
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/createCharacterPage.css">
+</head>
 
+<body>
 <div class="container">
-    <div class="row">
-        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-            <div class="card card-signin my-5">
-                <div class="card-body">
-                    <h5 class="card-title text-center">Prihlásenie</h5>
-                    <div class="text-center text-danger mb-3">
-                        <?= @$data['message'] ?>
-                    </div>
-                    <form class="form-signin" method="post" action="<?= $link->url("login") ?>">
-                        <div class="form-label-group mb-3">
-                            <input name="login" type="text" id="login" class="form-control" placeholder="Login"
-                                   required autofocus>
-                        </div>
-
-                        <div class="form-label-group mb-3">
-                            <input name="password" type="password" id="password" class="form-control"
-                                   placeholder="Password" required>
-                        </div>
-                        <div class="text-center">
-                            <button class="btn btn-primary" type="submit" name="submit">Prihlásiť
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+    <div class="header-text text-center">
+        <h2>Prihlásenie</h2>
     </div>
+    <form class="form-signin" method="post" action="<?= $link->url("login") ?>">
+        <div class="text-center text-danger mb-3">
+            <?= @$data['message'] ?>
+        </div>
+        <div class="mb-3">
+            <label for="login" class="form-label">Login:</label>
+            <input name="login" type="text" id="login" class="form-control" placeholder="Login" required autofocus>
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password:</label>
+            <input name="password" type="password" id="password" class="form-control" placeholder="Password" required>
+        </div>
+        <div class="text-center">
+            <button class="btn btn-dark" type="submit" name="submit">Prihlásiť</button>
+        </div>
+    </form>
 </div>
+</body>
+</html>
--- App/Views/Auth/register.view.php
+++ App/Views/Auth/register.view.php
@@ -9,35 +9,36 @@
 <html lang="sk">
 <head>
     <meta charset="UTF-8">
-    <title>Create Character</title>
+    <title>Register</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/createCharacterPage.css">
-    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">-->
 </head>
 <body>
-<form class="form" method="post" action="<?= $link->url('auth.save_user') ?>" id="formId" enctype="multipart/form-data">
-    <h3>Register</h3>
-    <div class="text-center text-danger mb-3">
-        <?= @$data['errors'][0] ?>
+<div class="container">
+    <div class="header-text">
+        <h2>Register</h2>
     </div>
-    <div class="mb-3">
-        <label for="username" class="form-label">Username:</label>
-        <input type="text" class="form-control" id="username" name="username"
-               placeholder="Username">
-    </div>
-    <div class="mb-3">
-        <label for="password" class="form-label">Password:</label>
-        <input type="text" class="form-control" id="password" name="password" placeholder="Password">
-    </div>
-    <div class="mb-3">
-        <label for="confirm-password" class="form-label">Confirm Password:</label>
-        <input type="text" class="form-control" id="confirm-password" name="confirm-password" placeholder="Confirm Password">
-    </div>
-
-    <button type="submit" id="register" class="btn btn-primary">Register</button>
-</form>
-
+    <form class="form" method="post" action="<?= $link->url('auth.save_user') ?>" id="formId" enctype="multipart/form-data">
+        <div class="text-center text-danger mb-3">
+            <?= @$data['errors'][0] ?>
+        </div>
+        <div class="mb-3">
+            <label for="username" class="form-label">Username:</label>
+            <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password:</label>
+            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+        </div>
+        <div class="mb-3">
+            <label for="confirm-password" class="form-label">Confirm Password:</label>
+            <input type="password" class="form-control" id="confirm-password" name="confirm-password" placeholder="Confirm Password">
+        </div>
+        <div class="text-center">
+            <button type="submit" id="register" class="btn btn-dark">Register</button>
+        </div>
+    </form>
+</div>
 <script src="public/js/registerPage_script.js"></script>
-
 </body>
 </html>
