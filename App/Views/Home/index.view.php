52e5fc73b9f59e69091b9af5a2da04ba6c91928b
MarekJedinak <jedinakmarek01@gmail.com>
2025-02-05 23:53:53 +0100

    HTML5 + API + DaisyUI +Tailwind

--- App/Views/Home/index.view.php
+++ App/Views/Home/index.view.php
@@ -14,15 +14,12 @@
     <title>Hlavna Stranka</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/homePage.css">
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.23/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
 
 <div class="telo">
-    <div class="popis-stranky">
-        <p>Welcome to <span>ScrambledEgg</span></p>
-        <p>This is a webpage created by a fan of DND and character creation.
-            Use it to share your creations with other members of the community.</p>
-    </div>
     <div class="sekcia-COTD">
         <h2>Character Of The Day:</h2>
         <div class="character-of-the-day">
@@ -35,12 +32,29 @@
             </div>
         </div>
     </div>
-    <p>Displaying location using Latitude and Longitude</p>
 
-    <button class="geeks" onclick="getlocation()">
-        Click Me
-    </button>
-    <p id="demo1"></p>
+    <div class="popis-stranky">
+        <p>Welcome to <span>ScrambledEgg</span></p>
+        <p>This is a webpage created by a fan of DND and character creation.
+            Use it to share your creations with other members of the community.</p>
+    </div>
+
+    <div class="card bg-white w-96 h-50 mt-12 shadow-2xl p-5 bg-cover bg-center rounded-xl relative overflow-hidden">
+
+        <div class="absolute inset-0  bg-opacity-40 backdrop-blur-lg"
+             style="background-image: url('public/images/weather.jpg'); background-size: 400px 325px"></div>
+
+        <div class="relative z-10 text-black text-center">
+            <h2 class=" text-2xl font-bold">🌡 Aktuálna teplota</h2>
+            <p class="mt-2"><strong>Mesto:</strong> <span id="city1">Načítavam...</span></p>
+            <p><strong>Lat:</strong> <span id="lat">-</span></p>
+            <p><strong>Long:</strong> <span id="long">-</span></p>
+            <p class="text-lg font-semibold mt-2"><strong>Teplota:</strong> <span id="temperature">--</span>°C</p>
+
+            <button class="btn btn-primary mt-4 w-full text-lg font-semibold" onclick="getlocation()">📍 Skontroluj počasie</button>
+        </div>
+    </div>
+
 
 
 </div>
--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,56 @@
 const profileBtn = document.getElementById('profileBtn');
 const profileDropdown = document.getElementById('profileDropdown');
 
+
+function getCityFromCoordinates(lat, lon) {
+    const apiUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`;
+    let location = "";
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.address) {
+                location = data.address.city || data.address.town || data.address.village || data.address.county || data.address.state || "Neznáme miesto";
+                console.log("Mesto / miesto:", location);
+                console.log(data);
+                document.getElementById("city1").textContent = location;
+
+
+            } else {
+                console.log("Mesto sa nenašlo.");
+            }
+        })
+        .catch(error => {
+            console.error("Chyba pri načítaní API:", error);
+        });
+    return location;
+}
+
 let variable1 = document.getElementById("demo1");
 function getlocation() {
     navigator.geolocation.getCurrentPosition(showLoc);
 }
 function showLoc(pos) {
-    variable1.innerHTML =
-        "Latitude: " +
-        pos.coords.latitude +
-        "<br>Longitude: " +
-        pos.coords.longitude;
+    const lat = pos.coords.latitude;
+    const long = pos.coords.longitude;
+    const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const temperature = data.hourly.temperature_2m[0];
+            document.getElementById("temperature").textContent = temperature;
+            document.getElementById("lat").textContent = lat;
+            document.getElementById("long").textContent = long;
+            getCityFromCoordinates(lat, long);
+
+        })
+        .catch(error => {
+            document.getElementById("temperature").textContent = "Chyba";
+            console.error("Chyba pri načítaní dát:", error);
+        });
+
 }
+
 
 profileBtn.addEventListener('click', () => {
     if (profileDropdown.style.display === 'block') {
